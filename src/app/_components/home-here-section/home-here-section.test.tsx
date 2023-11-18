d1f697060198083c68bca2a8f435d850acaaf2ea
suria babaei <suriyalifework60@gmail.com>
2023-11-18 15:58:16 +0330

    create home test

---
+++ src/app/_components/home-here-section/home-here-section.test.tsx
@@ -0,0 +1,27 @@
+import { render } from "@testing-library/react";
+import { describe } from "node:test";
+import { HomeHereSection } from "./home-here-section";
+
+describe('home here section', () => {
+    test('display the home here section', () => {
+       const {container} =  render(<HomeHereSection />);
+       expect(container).toMatchSnapshot(`
+       <section className="bg-hero-pattern mt-5 xl:mt-10 xl:bg-left">
+       <div className="container flex flex-col-reverse items-center xl:flex-row">
+           <div className="flex flex-col gap-5 mt-12 pb-5 text-center xl:text-right">
+               <h3 className="text-xl dark:text-info xl:text-2xl">خوش آمدید...</h3>
+               <h1 className="text-2xl font-black gradient lg:text-4xl xl:text-4xl">مسیر صعود به قله های برنامه نویسی</h1>
+               <p>هر جای مسیر برنامه نویسی که باشی با هم راهی استاد های با تجربه کلاسبن می تونی بدون محدودیت به قله های بالاتری صعود کنی  ما همیشه هواتو داریم</p>
+               <div className="mt-5 flex gap-4">
+                   <Button variant="primary" size="large">دوره های ریکت و نکست</Button>
+                   <Button variant="neutral" size="large">مشاوره برنامه نویسی</Button>
+
+               </div>
+               <Image src='/images/frameworks.png' width={412} height={39} alt="" className="grayscale mt-4 opacity-70 m-auto xl:m-0" />
+           </div>
+           <Image src='/images/programmer-landing.svg' width={702} height={521} alt="کلاسبن" />
+       </div>
+   </section>
+       `)
+    })
+})

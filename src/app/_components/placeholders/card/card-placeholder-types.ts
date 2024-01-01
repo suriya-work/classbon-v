9d91a093773d651102541506efa4922f47d88ebb
suriya babaei <suriyalifework60@gmail.com>
2024-01-01 11:07:31 +0330

    add components placeholder and testimonial

---
+++ src/app/_components/placeholders/card/card-placeholder-types.ts
@@ -0,0 +1,5 @@
+
+export type CardPalceholderProps =  {
+    count?: number;
+    className?:string;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,10 @@ import Feature from "./_components/feature/feature"; import { IconArrowLeftFill 
 import { BlogPostSummary } from "@/types/blog-post-summary.interface";
 import { BlogPostCardList } from "./(blog)/_components/blog-post-card-list";
 import { API_URL } from "@/configs/global";
+import { Suspense } from "react";
+import { CardPlaceholder } from "./_components/placeholders";
+import { TestimonialList } from "./_components/testimonial/testimonial-list";
+import { testimonials } from "@/data/testimonials";
 // this is getNewestCourses
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(`${API_URL}/courses/newest/${count}`, {
@@ -50,7 +54,10 @@ export default async function Home() {
           <h2 className="text-2xl font-extrabold">تازه ترین دوره های آموزشی</h2>
           <p>برای به روز بودن یادگرفتن نکته های تازه ضروریه</p>
         </div>
-        <CourseCardList courses={newestCourses} />
+        <Suspense fallback={<CardPlaceholder count={4} className="mt-5" />}>
+
+          <CourseCardList courses={newestCourses} />
+        </Suspense>
       </section>
       <section className="px-2 my-40">
         {/* <div className="sticky top-0 pt-0 text-center"> */}
@@ -113,6 +120,18 @@ export default async function Home() {
         </div>
         <BlogPostCardList posts={newestBlogPosts} />
       </section>
+      <div className="relative mt-32">
+        <div className="bg-primary pointer-events-none absolute bottom-0 left-1/2 aspect-square w-1/2 -translate-x-1/2 rounded-full opacity-5 -top-52 blur-3xl"></div>
+        <h2 className="text-info relative z-0 mx-auto text-3xl font-extrabold block w-fit">
+          <span className="-z-10 w-8 h-8 absolute bg-info opacity-25 -top-2 rounded-full inline-block -right-3"></span>
+          تجربه هم‌میسرهای کلاسبن
+        </h2>
+        <p className=" mb-32 text-lg text-center mt-2">
+          تو اینجا تنها نیستی. ببین هم‌مسیرهات نظرشون در مورد دوره‌های کلاسبن
+          چیه
+        </p>
+        <TestimonialList testimonials={testimonials} />
+      </div>
     </>
   )
 }

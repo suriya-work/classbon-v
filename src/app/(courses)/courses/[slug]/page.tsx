7b4ce009cd14b3c22bfb3bb4ed9c281090a1b6b0
suriya babaei <suriyalifework60@gmail.com>
2024-01-08 14:06:12 +0330

    change page abd add components

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -3,6 +3,10 @@ import { Rating } from "@/app/_components/rating/rating";
 import { API_URL } from "@/configs/global";
 import { CourseDetails } from "@/types/course-details.interface";
 import { CourseAside } from "./_components/course-aside";
+import { Tab } from "@/types/tab.type";
+import { Tabs } from "@/app/_components/tabs";
+import { Accordion as AccordionType } from "@/types/accordion";
+import { Accordion } from "@/app/_components/accordion";
 
 async function generateStaticParams() {
     const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
@@ -23,7 +27,29 @@ async function getCourse(slug: string): Promise<CourseDetails> {
 
 export default async function CoursesDetails({ params }: { params: { slug: string } }) {
     const { slug } = params;
-    const course = await getCourse(slug)
+    const course = await getCourse(slug);
+
+    const faqs : AccordionType[] = course.frequentlyAskedQuestions.map(
+        faq => ({
+            id: faq.id,
+            title: faq.question,
+            content: faq.answer,
+        })
+    )
+    const tabs: Tab[] = [
+        {
+            label: "مشخصات دوره",
+            content: course.description,
+        },
+        {
+            label: "دیدگاه‌ها و پرسش",
+            content: "course comments",
+        },
+        {
+            label: "سوالات متداول",
+            content: <Accordion data={faqs} />,
+        },
+    ];
     return (
         <div className="container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
             <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
@@ -38,9 +64,11 @@ export default async function CoursesDetails({ params }: { params: { slug: strin
                 <div className=" mt-5">Video Player Component</div>
             </div>
             <div className="col-span-10 xl:col-span-3">
-                <CourseAside {...course}/>
+                <CourseAside {...course} />
             </div>
-            <div className="col-span-10 xl:col-span-6 bg-info"></div>
+            <div className="col-span-10 xl:col-span-6">
+                <Tabs tabs={tabs} />
+            </div>
             <div className="col-span-10 xl:col-span-4 bg-warning"></div>
 
         </div>

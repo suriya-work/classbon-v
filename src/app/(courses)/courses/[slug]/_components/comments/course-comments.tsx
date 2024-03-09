f41f5795f01fd78f19887be9be296afac3328fdb
suriya babaei <suriyalifework60@gmail.com>
2024-03-09 11:49:42 +0330

    add comments

---
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { useParams } from "next/navigation";
+import { useCourseComments } from "../../_api/get-comments";
+const CourseComments = () => {
+    const { slug } = useParams();
+    const { data: comments } = useCourseComments({
+        params: {
+            slug: slug as string,
+            page: 1,
+        },
+    });
+    return (
+        <>
+            {comments?.data.map((p) => (
+                <p key={p.id} className="mb-8">{p.commentText}</p>
+            ))}
+        </>
+    );
+};
+
+export default CourseComments;

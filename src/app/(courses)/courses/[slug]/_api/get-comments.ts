be449cb2036c7c6066dcb89ac818f18de87bfc25
suriya babaei <suriyalifework60@gmail.com>
2024-04-21 14:13:53 +0330

    update react-query

--- src/app/(courses)/courses/[slug]/_api/get-comments.ts
+++ src/app/(courses)/courses/[slug]/_api/get-comments.ts
@@ -1,6 +1,6 @@
 import { readData } from "@/core/http-service/http-service";
 import { CourseCommentList } from "../_types/course-comments-interface";
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 
 type GetCommentsOptions = {
   params: {
@@ -21,6 +21,8 @@ export const useCourseComments = ({ params }: GetCommentsOptions) => {
   const { data } = useQuery({
     queryKey: ["courseComments"],
     queryFn: () => getComments({ params }),
+    staleTime: 5 * 60 * 60 * 1000,
+    gcTime: 6 * 60 * 60 * 1000,
   });
 
   return { data };

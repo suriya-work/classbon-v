01d27bce5899ec9639e2ab2a9902c30bd969a7ac
suriya babaei <suriyalifework60@gmail.com>
2024-04-22 12:34:39 +0330

    add interface comment

---
+++ src/types/comment.interface.ts
@@ -0,0 +1,10 @@
+export interface Comment {
+    id: number;
+    date: string;
+    userId: number | undefined;
+    fullName: string;
+    commentText: string;
+    score: number | null;
+    isResponse: boolean;
+  }
+  

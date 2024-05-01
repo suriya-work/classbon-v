7067f9e952a6b39509c7b73935e72b3d8b3a17e3
suriya babaei <suriyalifework60@gmail.com>
2024-05-01 09:57:06 +0330

    add textPlaceholder

--- src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
+++ src/app/(courses)/courses/[slug]/_components/comments/course-comments.tsx
@@ -2,9 +2,10 @@
 import { useParams } from "next/navigation";
 import { useCourseComments } from "../../_api/get-comments";
 import { Comment } from "@/app/_components/comment";
+import { TextPlaceholder } from "@/app/_components/placeholders";
 const CourseComments = () => {
     const { slug } = useParams();
-    const { data: comments } = useCourseComments({
+    const { data: comments , isLoading} = useCourseComments({
         params: {
             slug: slug as string,
             page: 1,
@@ -15,6 +16,9 @@ const CourseComments = () => {
             {comments?.data.map((comment) => (
                 <Comment key={`comment-${comment.id}`} {...comment} variant="info" />
             ))}
+            {
+                isLoading && <TextPlaceholder />
+            }
         </>
     );
 };

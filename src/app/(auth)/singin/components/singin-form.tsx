684b7536b74008695bf00b67eef6f6f8ba0a09b1
suriya babaei <suriyalifework60@gmail.com>
2024-07-27 12:28:01 +0330

    add react-hook-form

--- src/app/(auth)/singin/components/singin-form.tsx
+++ src/app/(auth)/singin/components/singin-form.tsx
@@ -2,20 +2,39 @@
 
 import { Button } from "@/app/_components/button/button";
 import { Textbox } from "@/app/_components/textbox";
+import { SignIn } from "../types/signin.types";
+import { useForm } from "react-hook-form";
 
 const SignInForm = () => {
-  return (
-    <>
-      <h5 className="text-2xl">ورود | ثبت نام</h5>
-      <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
-      <form className="flex flex-col gap-6 mt-16">
-        <Textbox placeholder="شماره موبایل"/>
-        <Button type="submit" variant="primary">
-          تایید و دریافت کد
-        </Button>
-      </form>
-    </>
-  );
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SignIn>();
+
+    const onSubmit = (data: any) => {
+        console.log(data);
+    }
+
+    return (
+        <>
+            <h5 className="text-2xl">ورود | ثبت نام</h5>
+            <p className="mt-2">
+                دنیای شگفت انگیز برنامه نویسی در انتظار شماست!
+            </p>
+            <form className="flex flex-col gap-6 mt-16" onSubmit={handleSubmit(onSubmit)}>
+                <Textbox
+                    {...register("mobile", {
+                        required: "شماره موبایل الزامی است",
+                    })}
+                    placeholder="شماره موبایل"
+                />
+                <Button type="submit" variant="primary">
+                    تایید و دریافت کد
+                </Button>
+            </form>
+        </>
+    );
 };
 
 export default SignInForm;

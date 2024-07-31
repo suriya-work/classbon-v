649dfda3eadc0830213895c2e2f1c427cf20d8a8
suriya babaei <suriyalifework60@gmail.com>
2024-07-31 10:58:18 +0330

    add signIn-form

--- src/app/(auth)/singin/components/singin-form.tsx
+++ src/app/(auth)/singin/components/singin-form.tsx
@@ -6,48 +6,60 @@ import { SignIn } from "../types/signin.types";
 import { useForm } from "react-hook-form";
 // import { TextInput } from "@/app/_components/form-input";
 import TextInput from "@/app/_components/form-input/text-input/text-input";
+import { useSignIn } from "../_api/signin";
+import { useRouter } from "next/navigation";
 
 const SignInForm = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors },
-    } = useForm<SignIn>();
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm<SignIn>();
 
-    const onSubmit = (data: any) => {
-        console.log(data);
-    }
+  const router = useRouter();
 
-    return (
-        <>
-            <h5 className="text-2xl">ورود | ثبت نام</h5>
-            <p className="mt-2">
-                دنیای شگفت انگیز برنامه نویسی در انتظار شماست!
-            </p>
-            <form className="flex flex-col gap-6 mt-16" onSubmit={handleSubmit(onSubmit)}>
-                <TextInput<SignIn>
-                  register={register}
-                  name={"mobile"}
-                  rules={{
-                    required: 'شماره موبایل الزامی است',
-                    maxLength: {
-                        value: 11,
-                        message: 'شماره موبایل باید 11 رقم باشد'
-                      },
-                      minLength: {
-                        value: 11,
-                        message: 'شماره موبایل باید 11 رقم باشد'
-                      }
-                  }}
-                  errors={errors}
-                />
+  const signIn = useSignIn({
+    onSuccess: () => {
+      router.push(`/verify?mobile = ${getValues("mobile")}`);
+    },
+  });
 
-                <Button type="submit" variant="primary">
-                    تایید و دریافت کد
-                </Button>
-            </form>
-        </>
-    );
+  const onSubmit = (data: SignIn) => {
+    signIn.submit(data);
+  };
+
+  return (
+    <>
+      <h5 className="text-2xl">ورود | ثبت نام</h5>
+      <p className="mt-2">دنیای شگفت انگیز برنامه نویسی در انتظار شماست!</p>
+      <form
+        className="flex flex-col gap-6 mt-16"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <TextInput<SignIn>
+          register={register}
+          name={"mobile"}
+          rules={{
+            required: "شماره موبایل الزامی است",
+            maxLength: {
+              value: 11,
+              message: "شماره موبایل باید 11 رقم باشد",
+            },
+            minLength: {
+              value: 11,
+              message: "شماره موبایل باید 11 رقم باشد",
+            },
+          }}
+          errors={errors}
+        />
+
+        <Button type="submit" variant="primary" isLoading={signIn.isPending}>
+          تایید و دریافت کد
+        </Button>
+      </form>
+    </>
+  );
 };
 
 export default SignInForm;

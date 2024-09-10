c2566e82a4bc256fad5395c3889ff849fb671cc6
suriya babaei <suriyalifework60@gmail.com>
2024-09-10 07:28:44 +0330

    add

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -71,3 +71,73 @@ export function generateID(): string {
 // export function cn(...inputs: ClassValue[]) {
 //   return twMerge(clsx(inputs));
 // }
+
+
+// import React, { ComponentPropsWithRef, ForwardedRef, ReactNode } from "react";
+// import { cn } from "@/utils";
+
+// interface IInputProps extends ComponentPropsWithRef<"input"> {
+//   error?: boolean;
+//   helperText?: string;
+//   label?: string;
+//   icon?: ReactNode;
+//   ref: ForwardedRef<HTMLInputElement>;
+//   labelClassName?: string;
+// }
+
+// function Input({
+//   error,
+//   helperText,
+//   label,
+//   disabled,
+//   name,
+//   icon,
+//   ref,
+//   className,
+//   labelClassName,
+//   dir = "rtl",
+//   ...rest
+// }: IInputProps) {
+//   return (
+//     <div className="flex flex-col">
+//       {label && (
+//         <label
+//           className={`mb-0 text-right text-sm text-black ${labelClassName}`}
+//           htmlFor={name}
+//         >
+//           {label}
+//         </label>
+//       )}
+//       <div className="relative  flex  items-center justify-start  ">
+//         {icon && (
+//           <div className="max-w-5 absolute right-1 top-1/2 -translate-y-1/2  overflow-hidden ">
+//             {icon}
+//           </div>
+//         )}
+//         <input
+//           {...rest}
+//           className={cn(
+//             " border-sm rounded-sm border-gray px-2 py-[6px] text-xs placeholder:text-primary focus:border-secondary focus:outline-none focus:ring-0",
+//             {
+//               "pr-6": icon,
+//               "border-danger": error,
+//               "bg-lightGray": disabled,
+//               " placeholder:text-right": dir === "rtl",
+//               " placeholder:text-left": dir === "ltr",
+//             },
+
+//             className,
+//           )}
+//           ref={ref}
+//         />
+//       </div>
+//       {helperText && (
+//         <span className={cn("text-xs text-primary", { "text-danger": error })}>
+//           {helperText}
+//         </span>
+//       )}
+//     </div>
+//   );
+// }
+
+// export const TextInput = React.forwardRef(Input);

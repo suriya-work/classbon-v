1307424d2467f92bb0721243cd02f14fc07f1ea9
suriya babaei <suriyalifework60@gmail.com>
2024-09-17 17:03:46 +0330

    add slider

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -65,3 +65,80 @@ export function generateID(): string {
 // export default HomePage;
 
 
+// react-multi-carousel
+
+// import React from "react";
+// import Carousel from "react-multi-carousel";
+// import "react-multi-carousel/lib/styles.css";
+// import "./styles.css";
+
+// const Slider = ({ responsiveSettings, autoPlay = true, swipeable = true, draggable = true, showDots = true, infinite = true, partialVisible = false, children }) => {
+//   return (
+//     <div className="parent">
+//       <Carousel
+//         responsive={responsiveSettings}
+//         autoPlay={autoPlay}
+//         swipeable={swipeable}
+//         draggable={draggable}
+//         showDots={showDots}
+//         infinite={infinite}
+//         partialVisible={partialVisible}
+//         dotListClass="custom-dot-list-style"
+//       >
+//         {children}
+//       </Carousel>
+//     </div>
+//   );
+// };
+
+// export default Slider;
+
+
+// use componens
+
+// import React from "react";
+// import Slider from "./Slider";
+
+// const responsive = {
+//   desktop: {
+//     breakpoint: { max: 3000, min: 1024 },
+//     items: 4,
+//     slidesToSlide: 4 // optional, default to 1.
+//   },
+//   tablet: {
+//     breakpoint: { max: 1024, min: 768 },
+//     items: 3,
+//     slidesToSlide: 3 // optional, default to 1.
+//   },
+//   mobile: {
+//     breakpoint: { max: 767, min: 464 },
+//     items: 2,
+//     slidesToSlide: 1 // optional, default to 1.
+//   }
+// };
+
+// const App = () => {
+//   return (
+//     <div>
+//       <h1>Movie Carousel</h1>
+//       <Slider responsiveSettings={responsive}>
+//         {/* هر محتوایی که اینجا قرار دهید به عنوان یک اسلاید در کاروسل نمایش داده می‌شود */}
+//         <div className="slider">
+//           <img src="https://i2.wp.com/www.geeksaresexy.net/wp-content/uploads/2020/04/movie1.jpg?resize=600%2C892&ssl=1" alt="movie1" />
+//         </div>
+//         <div className="slider">
+//           <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-kids-movies-2020-call-of-the-wild-1579042974.jpg?crop=0.9760858955588091xw:1xh;center,top&resize=480:*" alt="movie2" />
+//         </div>
+//         <div className="slider">
+//           <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-movies-for-kids-2020-sonic-the-hedgehog-1571173983.jpg?crop=0.9871668311944719xw:1xh;center,top&resize=480:*" alt="movie3" />
+//         </div>
+//         <div className="slider">
+//           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQS82ET2bq9oTNwPOL8gqyoLoLfeqJJJWJmKQ&usqp=CAU" alt="movie4" />
+//         </div>
+//       </Slider>
+//     </div>
+//   );
+// };
+
+// export default App;
+
